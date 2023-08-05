1064afb9114037c92b79b78288c81fa3f6ca3314
Gor.Grigoryan <Gor.Grigoryan@zevit.net>
2023-08-05 17:45:34 +0400

    Add migration and diagram.txt

--- Data-Encryption-With-EFCore-Value-Converters/Program.cs
+++ Data-Encryption-With-EFCore-Value-Converters/Program.cs
@@ -1,21 +1,41 @@
 ﻿using Data_Encryption_With_EFCore_Value_Converters.DataAccess;
 using Data_Encryption_With_EFCore_Value_Converters.Helpers;
 using Data_Encryption_With_EFCore_Value_Converters.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data_Encryption_With_EFCore_Value_Converters
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             //Set up
             EncryptionExtension.SetEncryptionKey("M!kSyJWnoj4j7V*8");
 
-            var entity = new Entity("Hello World");
+            using var context = new AppDbContext();
 
-            var context = new AppDbContext();
+            //Migrate DB to latest version
+            await context.Database.MigrateAsync();
 
-            context.Entities.Add(entity);
+            //Create test data
+            for (int i = 0; i < 50; i++)
+            {
+                var entity = new Entity(sensitiveInformation: $"Sensitive data No {i}");
+
+                context.Entities.Add(entity);
+            }
+
+            //save entities
+            await context.SaveChangesAsync();
+
+
+            //Get created data
+            var entitiesFromDb = await context.Entities.ToListAsync();
+
+            foreach (var entity in entitiesFromDb)
+            {
+                Console.WriteLine(entity.SensitiveInformation);
+            }
         }
     }
 }
