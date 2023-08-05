15aed919bf9b47af0f34f7f86213a72a072ccbb5
Gor.Grigoryan <Gor.Grigoryan@zevit.net>
2023-08-05 15:17:18 +0400

    Add atribute to SensitiveInformation Property

--- Data-Encryption-With-EFCore-Value-Converters/Models/Entity.cs
+++ Data-Encryption-With-EFCore-Value-Converters/Models/Entity.cs
@@ -1,8 +1,12 @@
-﻿namespace Data_Encryption_With_EFCore_Value_Converters.Models
+﻿using Data_Encryption_With_EFCore_Value_Converters.Attributes;
+
+namespace Data_Encryption_With_EFCore_Value_Converters.Models
 {
     public class Entity
     {
         public Guid Id { get; private set; }
+
+        [EncryptProperty]
         public string SensitiveInformation { get; private set; }
 
         public Entity(string sensitiveInformation)
